79ed49d6cdce5a34aa577aca1fec5f3d5ebd4f82
toan <toan130812@gmail.com>
2023-11-23 08:39:59 +0700

    end

--- src/home.js
+++ src/home.js
@@ -1,5 +1,6 @@
 export default function Home() {
-    return <div style={{ width: '100vw', height: '100vh', justifyContent: 'center', alignItems: 'center',display:'flex' }}>
+    return <div style={{ width: '100vw', height: '100vh', justifyContent: 'center', alignItems: 'center', display: 'flex' }}>
         <h1>Event Log</h1>
+
     </div>
 }
